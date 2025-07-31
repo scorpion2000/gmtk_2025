3894176687d7762d8dd42da0224178cb60d8b31b
Gary Chen <gurugaryc@gmail.com>
2025-07-31 15:32:53 +0100

    fix: error in player.gd fixed

--- Player/player.gd
+++ Player/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 @export var speed:int
-func _physics_process(delta): 
+func _physics_process(_delta): 
 	var direction = Input.get_vector("left","right", "up", "down")
 	velocity = velocity.lerp(direction*speed, 0.10)
 	move_and_slide()
