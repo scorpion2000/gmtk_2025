2170f761fd3326f4f7f5ad0190acac7c4d2e6d8f
Gary Chen <gurugaryc@gmail.com>
2025-08-01 02:02:22 +0100

    upgrade: improved player controller + sprint

--- scripts/player.gd
+++
@@ -1,7 +0,0 @@
-extends CharacterBody2D
-
-@export var speed:int
-func _physics_process(_delta): 
-	var direction = Input.get_vector("left","right", "up", "down")
-	velocity = velocity.lerp(direction*speed, 0.10)
-	move_and_slide()
---
+++ scripts/player/player.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+@export var walk_speed: float = 100.0
+@export var sprint_speed: float = 180.0
+@export var acceleration: float = 12.0
+@export var friction: float = 15.0
+
+var current_speed: float
+
+func _physics_process(delta: float): 
+	var direction = Input.get_vector("left", "right", "up", "down")
+	var is_sprinting = Input.is_action_pressed("sprint")
+	var target_speed = sprint_speed if is_sprinting else walk_speed
+	current_speed = lerp(current_speed, target_speed, acceleration * delta)
+	if direction != Vector2.ZERO:
+		velocity = velocity.move_toward(direction * current_speed, acceleration * current_speed * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, friction * current_speed * delta)
+	
+	move_and_slide()
